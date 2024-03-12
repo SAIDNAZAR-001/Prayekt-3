1411186208d563324c821c4b6e6bdf2279ebad8f
9-zona <sg975833@gmail.com>
2024-03-12 16:33:30 +0500

    first commit

--- src/sections/components/Banner.jsx
+++ src/sections/components/Banner.jsx
@@ -1,31 +1,43 @@
-function Button(){
-    return (
-        <button className="banner-button">
-            <p className="banner-button-text">Join the Waitlist</p>
-        </button>
-    )
+function Button() {
+  return (
+    <button className="banner-button">
+      <p className="banner-button-text">Join the Waitlist</p>
+    </button>
+  );
 }
 
-function Banner(){
-    return(
-        <main className="main">
-            <div className="container">
-                <div className="banner">
-                    <div className="banner-content">
-                        <h1 className="banner-content__title">Take <br/> Care <br/>of You</h1>
-                        <p className="banner-content__subtitle">
-                        Meet the modern finance platform created for physicians, <br/> by physicians. Your path to financial independence is here.
-                        </p>
-                        <Button/>
-                    </div>
-                    <div className="banner-imge">
-                        <img src="./banner-imge/banner-imge 1.png" alt="banner-imge 1.png" id="banner-imge1"/>
-                        <img src="./banner-imge/banner-imge 2.png" alt="banner-imge 2.png" id="banner-imge2"/>
-                    </div>
-                </div>
-            </div>
-        </main>
-    )    
+function Banner() {
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="banner">
+          <div className="banner-content">
+            <h1 className="banner-content__title">
+              Take <br /> Care <br />
+              of You
+            </h1>
+            <p className="banner-content__subtitle">
+              Meet the modern finance platform created for physicians, <br /> by
+              physicians. Your path to financial independence is here.
+            </p>
+            <Button />
+          </div>
+          <div className="banner-imge">
+            <img
+              src="src/img/banner-imge/banner-imge1.png"
+              alt="banner-imge 1.png"
+              id="banner-imge1"
+            />
+            <img
+              src="src/img/banner-imge/banner-imge2.png"
+              alt="banner-imge 2.png"
+              id="banner-imge2"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Banner;
